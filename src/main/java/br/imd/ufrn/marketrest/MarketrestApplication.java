b91f4b3b38ab939e413d682892b137ee28534bc8
VictorBitt15 <victor.bmv@hotmail.com>
2022-05-27 23:30:07 -0300

    first commit

---
+++ src/main/java/br/imd/ufrn/marketrest/MarketrestApplication.java
@@ -0,0 +1,13 @@
+package br.imd.ufrn.marketrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketrestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/imd/ufrn/marketrest/MarketrestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.imd.ufrn.marketrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
