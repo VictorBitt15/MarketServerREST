9699db83da01191e4f1edcee9e882d63532f0120
VictorBitt15 <victor.bmv15@gmail.com>
2022-06-02 10:28:59 -0300

    update

--- src/main/java/br/imd/ufrn/marketrest/model/Market.java
+++ src/main/java/br/imd/ufrn/marketrest/model/Market.java
@@ -1,13 +1,10 @@
 package br.imd.ufrn.marketrest.model;
 
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 
 import lombok.Data;
 
@@ -25,10 +22,5 @@ public class Market {
     @Column(nullable = false)
     private String nome;
 
-    @OneToMany(mappedBy = "market_id")
-    private List<Produto> listProdutos;
-
-    public void addProduto(Produto produto){
-        this.listProdutos.add(produto);
-    }
+   
 }
--- src/main/java/br/imd/ufrn/marketrest/model/Produto.java
+++ src/main/java/br/imd/ufrn/marketrest/model/Produto.java
@@ -5,13 +5,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 
 import lombok.Data;
 
 @Entity
 @Data
 public class Produto {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -22,7 +22,5 @@ public class Produto {
     @Column(nullable = true)
     private Double preco; 
 
-    @ManyToOne
-    private Market market_id;
 
 }
